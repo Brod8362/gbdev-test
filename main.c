4b8980e198233fd0c2e8747bd28e7ada0a85fb23
Brod8362 <brod8362@gmail.com>
2021-02-09 15:29:23 -0600

    Put sprites into one file

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <gb/cgb.h>
 #include <stdio.h>
 
-#include "graphics/bird.c"
+#include "graphics/sprites.c"
 
 #include "tilemap/graphics.c"
 #include "tilemap/bg_tilemap.c"
@@ -23,7 +23,8 @@ void main() {
     short pos_x = 50;
     short pos_y = 70;
 
-    const short BIRD_ID = 0;
+    const short bird_sprite_id = 0;
+    const short health_sprites[] = {1,2,3};
 
     set_bkg_data(0, 18, BackgroundTileset);
     set_bkg_tiles(0, 0, BackgroundTilemapWidth, BackgroundTilemapHeight, BackgroundTilemap);
@@ -34,10 +35,19 @@ void main() {
     //sprite handler stuff
 
     SPRITES_8x8;
-    set_sprite_data(BIRD_ID,0,BirdGraphics);
-    set_sprite_tile(BIRD_ID,0);
-    set_sprite_palette(BIRD_ID, 1, birdPalette);
-    move_sprite(BIRD_ID, pos_x, pos_y);
+    set_sprite_data(0,0,SpritesTLE0);
+    set_sprite_data(1,0,SpritesTLE2);
+
+    set_sprite_tile(bird_sprite_id,0);
+    set_sprite_palette(0, 1, birdPalette);
+    move_sprite(bird_sprite_id, pos_x, pos_y);
+
+    //create health sprites and put them on screen
+    for (int i = 0; i < 3; i++) {
+        int id = health_sprites[i];
+        set_sprite_tile(id, 1);
+        move_sprite(id, (i+1)*9, 15);
+    }
 
     SHOW_SPRITES;
 
@@ -63,7 +73,7 @@ void main() {
                 break;
         }
 
-        move_sprite(BIRD_ID, pos_x, pos_y);
+        move_sprite(bird_sprite_id, pos_x, pos_y);
         scroll_bkg(frame%2, 0);
         wait_vbl_done();
         frame++;
